e91a16b13fb46778fb3794b61afa834a767e34e5
Rrrrein <akunbuatambis@gmail.com>
2024-11-08 11:01:52 +0700

    perbaikan mongonya

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,19 @@ import connectDB from "./db-connection";
 import bookrouter from "./routes/book.route";
 
 const app = express();
+const PORT = 5000;
+
 
 app.use(express.json());
 connectDB();   
+
 // check endpoint
 app.get("/", (_, response) => {
   response.status(200).send("Server is up and running 💫");
 });
 
 app.use("/books", bookrouter);
-const PORT = 4000;
+
 app.listen(PORT, () => {
-  console.log('Express is running on Port ${PORT}');
+  console.log(`Express is running on Port ${PORT}`);
 });
